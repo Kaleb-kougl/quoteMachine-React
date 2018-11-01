78f60c7649eaedc978fb6b92d7e7174e1d393361
Kaleb Kougl <kalebkougl@gmail.com>
2018-11-01 11:04:52 -0500

    Updates

---
+++ precache-manifest.9b2a345a4851a817fe992aaee2b5ad41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bcf4b685c302bda96f3",
+    "url": "/quoteMachine-React/static/js/runtime~main.8bcf4b68.js"
+  },
+  {
+    "revision": "418d22828c92328a60f7",
+    "url": "/quoteMachine-React/static/js/main.418d2282.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/quoteMachine-React/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "418d22828c92328a60f7",
+    "url": "/quoteMachine-React/static/css/main.646c7ff5.chunk.css"
+  },
+  {
+    "revision": "e8727c64f5759070df50959ff63880c8",
+    "url": "/quoteMachine-React/index.html"
+  }
+];
